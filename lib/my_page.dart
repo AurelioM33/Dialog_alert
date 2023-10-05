93efba088f1b2becfea13bf4017d106db3d092aa
Marco Aurelio <m.aurelio887@gmail.com>
2023-10-05 13:40:34 -0300

    Dialog_Alert

---
+++ lib/my_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MyPage extends StatefulWidget {
+  const MyPage({super.key});
+
+  @override
+  State<MyPage> createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: showMyDialog,
+          child: const Text('Show My Dialog'),
+        ),
+      ),
+    );
+  }
+
+  showMyDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text('Close'),
+          ),
+        ],
+        title: const Text('Code Flutter'),
+        content: const Text('This is my alert Dialog'),
+      ),
+    );
+  }
+}
